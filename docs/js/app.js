0ac7a5fd5ecb0de652cd87f4796ae807f2a5f8f3
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-07 07:59:43 +0900

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,15 +1,21 @@
 "use strict"
 
+
+document.addEventListener('DOMContentLoaded', viewportHandler);
+visualViewport.addEventListener('resize', viewportHandler);
+
+document.addEventListener('DOMContentLoaded', loadcanvas);
+
+
+
 //console.log(visualViewport.height);
 function viewportHandler(event) {
   document.documentElement.style.setProperty(
       '--visualViewport-height',
       `${visualViewport.height}px`
     );
-  //console.log(visualViewport.height);
   
   const targets = document.querySelectorAll('.log');
-  //targets.innerHTML = visualViewport.height;
   for (const target of targets) {
     target.innerHTML = visualViewport.height;
   }
@@ -17,16 +23,24 @@ function viewportHandler(event) {
 }
 
 
-document.addEventListener('DOMContentLoaded', viewportHandler);
-visualViewport.addEventListener('resize', viewportHandler);
 
 
+let myCanvas;
 
+function loadcanvas() {
+  myCanvas = document.querySelector('#mainCanvas');
+  //const { clientWidth : w , clientHeight : h } =  myCanvas;
+  const {width : w, height : h} =  myCanvas;
+  const ctx = myCanvas.getContext('2d');
+  ctx.fillStyle = 'maroon';
+  ctx.fillRect(0, 0, w, h);
+  ctx.beginPath();
+  ctx.strokeStyle = 'blue';
+  ctx.moveTo(0, 0); ctx.lineTo(w, h);
+  ctx.moveTo(w, 0); ctx.lineTo(0, h);
+  ctx.stroke();
+}
 
-
-const canvas = document.getElementById('mainCanvas');
-const ctx = canvas.getContext('2d');
-console.log(canvas);
 
 
 
