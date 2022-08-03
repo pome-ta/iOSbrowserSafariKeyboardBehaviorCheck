9d19cf0e453e2e4344092dad9250751c883fba0a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-03 20:16:16 +0900

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,2 +1,15 @@
-"use strict"
+//import { EditorView, basicSetup } from 'https://cdn.skypack.dev/codemirror';
 
+import {EditorState} from 'https://cdn.skypack.dev/@codemirror/state';
+import {EditorView} from 'https://cdn.skypack.dev/@codemirror/view';
+import {lineNumbers} from 'https://cdn.skypack.dev/@codemirror/gutter';
+
+let startState = EditorState.create({
+  doc: "Hello World",
+  //extensions: [lineNumbers()]
+})
+
+let view = new EditorView({
+  state: startState,
+  parent: document.body,
+});
