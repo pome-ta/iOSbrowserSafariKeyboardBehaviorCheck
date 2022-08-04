139c97884171c5cf1ece791681ea662eab6ade0e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-04 16:35:20 +0900

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,32 +1,8 @@
-//import { EditorView, basicSetup } from 'https://esm.sh/codemirror';
+import { EditorView, basicSetup } from 'https://esm.sh/codemirror';
 import { javascript } from 'https://esm.sh/@codemirror/lang-javascript';
 
 
-
-import { EditorState } from 'https://esm.sh/@codemirror/state';
-import { EditorView, keymap } from 'https://esm.sh/@codemirror/view';
-import { defaultKeymap } from 'https://esm.sh/@codemirror/commands';
-
-
-
-
-//import { cpp } from 'https://esm.sh/@codemirror/lang-cpp';
-
-
-
-const editorDiv = document.createElement('div');
-editorDiv.id = 'editorWrap';
-editorDiv.style.width = '100%';
-document.body.appendChild(editorDiv);
-
-
-let startState = EditorState.create({
-  doc: "Hello World",
-  extensions: [keymap.of(defaultKeymap)]
+let view = new EditorView({
+  extensions: [basicSetup, javascript()],
+  parent: document.body
 })
-
-
-let editor = new EditorView({
-  state: startState,
-  parent: document.querySelector('#editorWrap'),
-});
