827b7f1b411dfdbab21a061d81f3238620b97e14
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-08 00:00:21 +0900

    whitespace

--- docs/js/app.js
+++ docs/js/app.js
@@ -8,6 +8,7 @@ import {
   dropCursor,
   highlightActiveLine,
   keymap,
+  highlightSpecialChars,
 } from '@codemirror/view';
 import { indentOnInput, bracketMatching } from '@codemirror/language';
 import { highlightSelectionMatches } from '@codemirror/search';
@@ -18,7 +19,9 @@ import {
   completionKeymap,
 } from '@codemirror/autocomplete';
 
-import { javascript } from '@codemirror/lang-javascript';
+//import { javascript } from '@codemirror/lang-javascript';
+import { python } from '@codemirror/lang-python';
+
 import { oneDark } from '@codemirror/theme-one-dark';
 import { indentationMarkers } from '@replit/codemirror-indentation-markers';
 
@@ -30,7 +33,14 @@ editorDiv.style.width = '100%';
 //editorDiv.style.height = '100%';
 document.body.appendChild(editorDiv);
 
-const codeSample = ``;
+const codeSample = `import math
+
+def calc_distance(x1, y1, x2, y2):
+    diff_x = x1 - x2
+    diff_y = y1 - y2
+
+    return math.sqrt(diff_x**2 + diff_y**2)
+`;
 
 const myTheme = EditorView.baseTheme({
   '&.cm-editor': {
@@ -42,10 +52,21 @@ const myTheme = EditorView.baseTheme({
   },
 });
 
+//const tabSize = new Compartment();
 
-const tabSize = new Compartment();
+const myHiChars = highlightSpecialChars({
+  render: (code) => {
+    let node = document.createElement('span');
+    node.innerText = '•';
+    node.title = '\\u' + code.toString(16);
+    console.log(node);
+    return node;
+  },
+  //specialChars: /[\x01-\x08\x0B-\x1F]/g
+});
 
 const state = EditorState.create({
+  doc: codeSample,
   extensions: [
     minimalSetup,
     /* diff basicSetup */
@@ -60,18 +81,18 @@ const state = EditorState.create({
     autocompletion(),
     keymap.of([...closeBracketsKeymap, ...completionKeymap]),
     /* --- basicSetup */
-    tabSize.of(EditorState.tabSize.of(2)),
+    //tabSize.of(EditorState.tabSize.of(4)),
     EditorView.lineWrapping, // 改行
-    javascript(),
-    oneDark, // theme
-    myTheme, // custom
-    indentationMarkers(),
-  ]
-})
-
+    python(),
+    //oneDark, // theme
+    //myTheme, // custom
+    //indentationMarkers(),
+    //highlightSpecialChars(),
+    myHiChars,
+  ],
+});
 
 const editor = new EditorView({
-  doc: codeSample,
   state,
   parent: editorDiv,
 });
